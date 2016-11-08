83f3ad593fdf7b396a0c0f58470089da9aae5cd3
Xenoguner <johnmercado2@gmail.com>
2016-11-08 22:59:20 +0000

    Argh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,10 +85,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    bucket: ENV.fetch('fiercetaigaimages'),
+    access_key_id: ENV.fetch('AKIAJ4XVKGFXEW7NZDOA'),
+    secret_access_key: ENV.fetch('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'),
   }
 }
   
